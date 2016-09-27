995977fe91e0ac5eb46a756bfde36ab65a24efd7
Greg Caldwell <caldwellgp@googlemail.com>
2016-09-27 11:02:05 +0100

    Initial tests for masking. Currently only working for single display object with a single display object as (graphics) mask.

--- openfl/_internal/renderer/canvas/CanvasGraphics.hx
+++ openfl/_internal/renderer/canvas/CanvasGraphics.hx
@@ -806,7 +806,8 @@ class CanvasGraphics {
 		
 		graphics.__update ();
 		
-		if (graphics.__dirty) {
+		var mask = graphics.__owner.mask;
+		if (graphics.__dirty || (mask != null && mask.__graphics.__dirty) ) {
 			
 			hitTesting = false;
 			
