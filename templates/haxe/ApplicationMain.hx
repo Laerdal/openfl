07c71d359e827694427826fd562d95587bdcd225
Greg Caldwell <caldwellgp@googlemail.com>
2017-01-11 10:20:16 +0000

    Added new <window httpReqTimeout="" > attribute to project.xml to allow overriding the default 30000 ms HttpRequest timeout - core functionality is in Lime

--- templates/haxe/ApplicationMain.hx
+++ templates/haxe/ApplicationMain.hx
@@ -32,6 +32,7 @@ import DefaultAssetLibrary;
 			company: "::meta.company::",
 			file: "::APP_FILE::",
 			fps: ::WIN_FPS::,
+			httpReqTimeout: ::WIN_HTTP_REQ_TIMEOUT::,
 			name: "::meta.title::",
 			orientation: "::WIN_ORIENTATION::",
 			packageName: "::meta.packageName::",
