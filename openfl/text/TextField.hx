48f1f23df8d597814b1e5e2c2b97afeddf739790
Danny Wilson <danny@prime.vc>
2017-01-12 13:23:41 +0100

    DOM: implement text selection support

--- openfl/text/TextField.hx
+++ openfl/text/TextField.hx
@@ -2064,7 +2064,9 @@ class TextField extends InteractiveObject {
 			if (position != __caretIndex) {
 				
 				__caretIndex = position;
+				#if !dom
 				__dirty = true;
+				#end
 				
 			}
 			
@@ -2145,7 +2147,9 @@ class TextField extends InteractiveObject {
 		
 		__caretIndex = __getPosition (mouseX, mouseY);
 		__selectionIndex = __caretIndex;
+		#if !dom
 		__dirty = true;
+		#end
 		
 		stage.addEventListener (MouseEvent.MOUSE_MOVE, stage_onMouseMove);
 		stage.addEventListener (MouseEvent.MOUSE_UP, stage_onMouseUp);
